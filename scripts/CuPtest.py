88b55694d7deb55447405d1b4ac6ad7f85840465
mmayers88 <mmayers@pdx.edu>
2023-01-28 17:34:08 -0800

    programmed CuP asynchronously; untested.

--- scripts/CuPtest.py
+++ scripts/CuPtest.py
@@ -10,6 +10,9 @@ import sys
 import math
 import os
 
+#settings
+thresh_num = 48.0
+
 #set current binary state 1 or True
 xState = True
 
@@ -51,53 +54,139 @@ def drone_rot(rot = 0, r=60):
 		drone.attitude_control(DJIDrone.HORIZ_POS|DJIDrone.VERT_VEL|DJIDrone.YAW_ANG|DJIDrone.HORIZ_BODY|DJIDrone.STABLE_ON, 0, 0, 0, rot)
 		time.sleep(0.02)
 
+def init_state():
+	#take sample
+	gas_sensor_msg = GasSensor()
+	msg_a = gas_sensor_msg.data
+	#save coordinates
+	coor_a = (drone.local_position.x,drone.local_position.y)
+	#fly until finding the first  cut edge
+	while True:
+		#move up
+		for i in range(20):
+			drone.attitude_control(DJIDrone.HORIZ_POS|DJIDrone.VERT_VEL|DJIDrone.YAW_ANG|DJIDrone.HORIZ_BODY|DJIDrone.STABLE_ON, 2, 0, 0, 0)
+			time.sleep(0.02)
+		time.sleep(5)
+		#take second sample
+		gas_sensor_msg = GasSensor()
+		msg_b = gas_sensor_msg.data
+		#save coordinates
+		coor_b = (drone.local_position.x,drone.local_position.y)
+		#if local sample is below threshold of border
+		if msg_b < thresh_num:
+			#edge is found: start CuP
+			break
+		else:
+			#new edge is old edge, keep flying, try again
+			msg_a = msg_b
+			coor_a = coor_b
+	return msg_a, msg_b, coor_a, coor_b
+
+def rot_cell_points(origin, dir, buff = 10):
+	arr = np.array(
+		[[(origin.x, origin.y),(origin.x, origin.y+buff)],
+		[(origin.x+buff, origin.y+buff),(origin.x+buff, origin.y)]]
+	)
+	if dir == 0:
+		return arr
+	for i in range(dir):
+		arr = rotate(arr, arr[0], -90)
+	return arr
+
+			
+def CuP(msg_a, msg_b, coor_a, coor_b):
+	while True:
+		#check edges
+		#find line orientation
+		dir = -1
+		x_dir  = coor_b[0] - coor_a[0]
+		if x_dir:
+			if x_dir > 0:
+				# a-b
+				dir = 1
+			else:
+				# b-a
+				dir = 3
+		else:
+			if (coor_b[1] - coor_a[1]) > 0:
+				# b
+				# |
+				# a
+				dir = 0
+			else:
+				# a
+				# |
+				# b
+				dir = 2
+		# get prospective points of node and rotate
+		cell_points = rot_cell_points(coor_a, dir)
+		drone.local_position_navigation_send_request(cell_points[2][0],cell_points[2][1],3)
+		time.sleep(10)
+		#take sample
+		msg_c = gas_sensor_msg.data
+		#check for edge
+		if msg_c > thresh_num:
+			msg_a = msg_c
+			coor_a = (cell_points[2][0],cell_points[2][1])
+		else:
+			#logic to move past next point
+			# check other point
+			drone.local_position_navigation_send_request(cell_points[3][0],cell_points[3][1],3)
+			time.sleep(10)
+			#take sample
+			msg_d = gas_sensor_msg.data
+			if msg_d > thresh_num:
+				msg_b = msg_c
+				msg_a = msg_d
+				coor_a = (cell_points[3][0],cell_points[3][1])
+				coor_b = (cell_points[2][0],cell_points[2][1])
+			else:
+				msg_b = msg_d
+				coor_b = (cell_points[3][0],cell_points[3][1])
+				write_msg = msg_a
+		#write coordinates
+		gps_x = drone.global_position.logitude
+		gps_y = drone.global_position.latitude,
+		GPS_coor = np.vstack((GPS_coor,[gps_x,gps_y,coor_a.x,coor_a.y,coor_b.x,coor_b.y]))
+		#check terminal state
+			#head home if done
+
+		#check for low power
+		if low_power(drone.power_status.percentage):
+			print("Drone Disarmed")
+			print("Low Power")
+			return GPS_coor
+
+	return GPS_coor
+	'''if xState:
+		#move
+		#check state
+		#if bounds are 
+	#fly in a square
+	drone.local_position_navigation_send_request(6,6,3)
+	#drone.local_position_control(3,3,3,0)
+	time.sleep(6)
+	drone.local_position_control(6,6,3,0)'''
+
+
 def main():
+	'''
 	for i in range(1):
 		#rospy.init_node('test_node')
 		print("========================")
 		print(GasSensor())
 		rospy.Subscriber("hku_m100_gazebo/GasSensor", GasSensor, callback)
 		print("===========END==========")
-		rospy.spin()
+		gas_sensor_msg = GasSensor()
+		print(gas_sensor_msg.data)
 	loc_x = drone.local_position.x
-	loc_y = drone.local_position.y
+	loc_y = drone.local_position.y'''
 
-	#init state: fly until finding the first  cut edge
-	while True:
-		#take sample
-		#save coordinates
-		#move
-		for i in range(60):
-			drone.attitude_control(DJIDrone.HORIZ_POS|DJIDrone.VERT_VEL|DJIDrone.YAW_ANG|DJIDrone.HORIZ_BODY|DJIDrone.STABLE_ON, 2, 2, 0, 0)
-			time.sleep(0.02)
-		time.sleep(5)
-		#take second sample
-		#save coordinates
-		#if local sample is below threshold of border
-			#edge is found: start CuP
-		#else
-			#new edge is old edge, keep flying, try again
+	# init state: this is a priori
+	msg_a, msg_b, coor_a, coor_b = init_state()
+	
 	#CuP
-	while True:
-		#check edges
-		#find old and new line orientation
-		# get prospective points of node and rotate
-		# check other two points
-		#find edge
-		#check for low power
-		if xState:
-			#move
-			#check state
-			#if bounds are 
-		#fly in a square
-		drone.local_position_navigation_send_request(6,6,3)
-		#drone.local_position_control(3,3,3,0)
-		time.sleep(6)
-		drone.local_position_control(6,6,3,0)
-		if low_power(drone.power_status.percentage):
-			print("Drone Disarmed")
-			print("Low Power")
-			return
+	GPS_data = CuP(msg_a, msg_b, coor_a, coor_b)
 
 
 if __name__ == "__main__":
@@ -130,6 +219,13 @@ if __name__ == "__main__":
 		print(drone.local_position)
 		
 		time.sleep(2)
+	GPS_coor = np.array([
+		drone.global_position.logitude, 
+		drone.global_position.latitude, 
+		drone.local_position.x, 
+		drone.local_position.y, 
+		drone.local_position.x, 
+		drone.local_position.y])
 	main()
 
 
